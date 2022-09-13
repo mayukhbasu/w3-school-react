39895d7ca52ab350722eaa5edbfa78475545b063
Bubul <bubul@Bubuls-MBP.Dlink>
2022-09-13 21:50:27 +0530

    created full screen video application final commit

--- src/Pages/FullScreenVideo/FullScreenVideo.jsx
+++ src/Pages/FullScreenVideo/FullScreenVideo.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './FullScreenVideo.css'
 
 const FullScreenVideo = () => {
+    const tekkenVideo = useRef();
+    const [buttonLabel, setButtonLabel] = useState(false);
+
+    const pause = () => {
+        setButtonLabel(!buttonLabel);
+        if(!buttonLabel) {
+            tekkenVideo.current.pause();
+        } else {
+            tekkenVideo.current.play();
+        }
+    }
     return (
         <>
-            <video autoPlay loop muted id='myVideo'>
+            <video autoPlay loop muted id='myVideo' ref={tekkenVideo}>
                 <source src='armorking.mp4' type='video/mp4'></source>
             </video>
             <div className='content'>
-                <button id='myButton'>Pause</button>
+                <button id='myButton' onClick={pause}>{buttonLabel === false ? 'Pause': 'Play'}</button>
             </div>
         </>
     );
