78b584d86d455c43995c3e0415879148609dc7c1
Bubul <bubul@Bubuls-MBP.Dlink>
2022-09-13 23:08:52 +0530

    created modals

--- src/Pages/Modal/Modal.jsx
+++ src/Pages/Modal/Modal.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './Modal.css';
 
 const Modal = () => {
+
+    const modal = useRef();
+    const open = () => {
+        modal.current.style.display = "block";
+    }
+    const closeModal = () => {
+        modal.current.style.display = "none";
+    }
+
+    const outsideClick = (e) => {
+        if(e.target.id === "modal") {
+            modal.current.style.display = "none";
+        }
+    }
     return (
         <>
-            <button className='button'>Open Modal</button>
-            <div className='modal'>
-                <div className="modal-content">
-                    <span className='close'>&times;</span>
+            <button className='button' onClick={open}>Open Modal</button>
+            <div className='modal' id='modal' ref={modal} onClick={outsideClick}>
+                <div className="modal-content" id='modal-content'>
+                    <span className='close' id='close' onClick={closeModal}>&times;</span>
                     <p>Some Text in the modal</p>
                 </div>
                 
